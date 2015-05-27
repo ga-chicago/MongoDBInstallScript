10ee56d184b945e57327147ed83d050f20c18bf7
James <code-for-coffee@users.noreply.github.com>
2015-05-27 00:11:03 -0500

    added brew update

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,6 +3,7 @@ colour='\033[1;35m'
 NC='\033[0m' # No Color
 echo -e "${colour}*** Hi! This script will install MongoDB."
 echo -e "You could be asked for your admin password up to four times."
+brew update
 # use homebrew to install 
 echo -e "Installing MongoDB"
 sudo brew install mongodb -Y
